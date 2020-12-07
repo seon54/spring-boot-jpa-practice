05890c1a403437e27c37edf5912d37a6e50ba6c2
shpark <jlk@shpark.local>
2020-12-07 11:17:05 +0900

    주문 조회 V4: JPA에서 DTO 직접 조회

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -60,10 +60,19 @@ public class OrderRepository {
 
     public List<Order> findAllWithItem() {
         return em.createQuery("select distinct o from Order o" +
-                                " join fetch o.member m" +
-                                " join fetch o.delivery d" +
-                                " join fetch o.orderItems oi" +
-                                " join fetch oi.item i", Order.class)
+                " join fetch o.member m" +
+                " join fetch o.delivery d" +
+                " join fetch o.orderItems oi" +
+                " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithMemberDeliver(int offset, int limit) {
+        return em.createQuery("select o from Order o" +
+                " join fetch o.member m" +
+                " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
                 .getResultList();
     }
 }
